e6bee8fd73e86ca195dd12c9bcec1d76cabbdbe4
Lario <osunkunleolanrewaju@gmail.com>
2022-07-21 23:59:09 +0100

    revised states and setstates by building a counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
-import { PostForm } from './components/PostForm';
-// import { ParentComponent } from './components/ParentComponent';
-// import { PostList } from './components/PostList';
-// import Greet from './components/Greet';
-// import Message from './components/Message';
+import Counter from './components/Counter';
+
 
 function App() {
+
   return (
     <div className="App">
-     {/* <Message /> */}
-     {/* <ParentComponent /> */}
-     < PostForm />
+      <Counter />
     </div>
   );
 }
